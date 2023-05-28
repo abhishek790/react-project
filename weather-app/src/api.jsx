5f5258e67864c8cb3d00d75c33f9c6cdcec7261e
abhishek790 <mishraavi790@gmail.com>
2023-05-28 16:30:06 +0545

    Delete weather-app directory

--- weather-app/src/api.jsx
+++
@@ -1,9 +0,0 @@
-export const geoApiOptions = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "02b06ff846mshdfcc6ed55033ad7p14d79djsnb6615909cb34",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
-};
-
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
