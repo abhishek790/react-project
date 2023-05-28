d306e519a486ec170aa14904ed4d036149cd9266
Abhishek Mishra <mishraavi790@gmail.com>
2023-05-28 22:40:05 +0545

    2nd change

--- weather-app/src/api.jsx
+++ weather-app/src/api.jsx
@@ -7,3 +7,6 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_API_KEY = "0a71e068bc5f5b3709bf3583e2f7b167";
