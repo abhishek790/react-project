ed2f0fbfc23c83e6231923ded57077ac9167f2b8
Abhishek Mishra <mishraavi790@gmail.com>
2023-06-03 17:20:33 +0545

    minor update

--- tour-and-travel-website/src/App.jsx
+++ tour-and-travel-website/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar";
+import Home from "./Routes/Home";
+import Service from "./Routes/Service";
+import About from "./Routes/About";
+import Contact from "./Routes/Contact";
 
 function App() {
   return (
-    <>
-      <div className="App">
+    <div className="App">
+      <Router>
         <Navbar />
-      </div>
-    </>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- tour-and-travel-website/src/components/Navbar.jsx
+++ tour-and-travel-website/src/components/Navbar.jsx
@@ -1,37 +1,34 @@
-import { Component } from "react";
+import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
+import { useState } from "react";
 import "./Navbar.css";
-class Navbar extends Component {
-  state = { clicked: false };
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+function Navbar() {
+  const [state, setState] = useState(false);
+  const handleClick = () => {
+    setState(!state);
   };
-  // render is needed because we are extending from class component(it is like a formula)
-  render() {
-    return (
-      <nav className="NavbarItems">
-        <h1 className="navbar-logo">Trippy</h1>
-        <div className="menu-icons" onClick={this.handleClick}>
-          <i
-            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-          ></i>
-        </div>
-        <ul className="nav-menu">
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href={item.url}>
-                  <i className={item.icon}></i>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-          <button>Sign Up</button>
-        </ul>
-      </nav>
-    );
-  }
+
+  return (
+    <nav className="NavbarItems">
+      <h1 className="navbar-logo">Trippy</h1>
+      <div className="menu-icons" onClick={handleClick}>
+        <i className={state ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={state ? "nav-menu active" : "nav-menu"}>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link className={item.cName} to={item.url}>
+                <i className={item.icon}></i>
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+        <button>Sign Up</button>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
