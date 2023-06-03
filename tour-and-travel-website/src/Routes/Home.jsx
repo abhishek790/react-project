c6aa32d8073f98763bfb7f3457cb6c8b3556116f
Abhishek Mishra <mishraavi790@gmail.com>
2023-06-03 19:38:36 +0545

    major changes

--- tour-and-travel-website/src/Routes/Home.jsx
+++ tour-and-travel-website/src/Routes/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Hero from "../components/Hero";
+import imgs from "../imgs/12.jpg";
 
 function Home() {
   return (
     <div>
-      <h1>This is Home</h1>
+      <Hero
+        cName="hero"
+        heroImg={imgs}
+        title="Your Journey Your Story"
+        text="Choose Your Favourite Destination."
+        buttonText="Travel Plan"
+        url="/"
+        btnClass="show"
+      />
     </div>
   );
 }
--- tour-and-travel-website/src/components/Hero.jsx
+++ tour-and-travel-website/src/components/Hero.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Herostyles.css";
 
-function Hero() {
-  return <div></div>;
+function Hero(props) {
+  return (
+    <>
+      <div className={props.cName}>
+        <img src={props.heroImg} alt="image" />
+
+        <div className="hero-text">
+          <h1>{props.title}</h1>
+          <p>{props.text}</p>
+          <a href={props.url} className={props.btnClass}>
+            {props.buttonText}
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Hero;
