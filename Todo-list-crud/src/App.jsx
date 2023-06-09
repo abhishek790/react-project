a902a49a84b48e5de4e4a6259ec360f8f403b9aa
Abhishek Mishra <mishraavi790@gmail.com>
2023-06-09 17:11:13 +0545

    major changes

--- Todo-list-crud/src/App.jsx
+++ Todo-list-crud/src/App.jsx
@@ -1,40 +1,69 @@
-import Todo from "./Todo/Todo";
 import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [todolist, setTodoList] = useState([]);
-  const [todoitem, setTodoItem] = useState("");
-  const [toggle, setToggle] = useState(true);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditid] = useState(0);
 
-  const Edit = (item) => {
-    inp.value = item.todoitem;
-    setToggle(!toggle);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditid(0);
+      setTodo("");
+      return; // we don't want to go beyound this for editing otherwise it will create another todolist
+    }
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo(" ");
+    }
   };
 
-  const addList = () => {
-    const add = {
-      id: Date.now(),
-      todoitem: todoitem,
-    };
-    setTodoList([...todolist, add]);
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
   };
 
-  const deleteItem = (id) => {
-    console.log(id);
-    setTodoList(todolist.filter((item) => (item.id !== id ? true : false)));
+  const handleEdit = (id) => {
+    //returns whole object that matches with the id
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditid(id);
   };
 
   return (
-    <>
-      <Todo
-        setTodoItem={setTodoItem}
-        addList={addList}
-        Toggle={Edit}
-        todolist={todolist}
-        toggle={toggle}
-        deleteItem={deleteItem}
-      />
-    </>
+    <div className="App">
+      <div className="container">
+        <h1>Todo List App</h1>
+        <form className="todoForm" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button>{editId ? "Edit" : "Go"}</button>
+        </form>
+        <ul className="allTodos">
+          {todos.map((t) => (
+            <li className="singleTodo">
+              <span className="todoText" key={t.id}>
+                {t.todo}
+              </span>
+              <button onClick={() => handleEdit(t.id)}>Edit</button>
+              <button onClick={() => handleDelete(t.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
