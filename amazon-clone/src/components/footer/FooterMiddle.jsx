788632eb8c40699b85af6ecb06d8d22b8935689d
Abhishek Mishra <mishraavi790@gmail.com>
2023-06-17 20:11:10 +0545

    minor changes

--- amazon-clone/src/components/footer/FooterMiddle.jsx
+++ amazon-clone/src/components/footer/FooterMiddle.jsx
@@ -3,9 +3,55 @@ import React from "react";
 function FooterMiddle() {
   return (
     <div className="w-full bg-amazon_light text-white">
-      <div className="w-full border-b-[1px] border-gray-500 py-100">
-        <div className="max-w-5xl mx-auto text-gray-300">
-          <h1></h1>
+      {/* top start here */}
+      <div className="w-full border-b-[1px] border-gray-500 py-10">
+        <div className="max-w-5xl mx-auto text-gray-500">
+          <div className="w-full grid grid-cols-4 place-items-center items-start">
+            <div>
+              <h3 className="font-titleFont text-white text-base font-semibold mb-3">
+                Get to know Us
+              </h3>
+              <ul className="flex flex-col gap-2 font-bodyFont">
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="font-titleFont text-white text-base font-semibold mb-3">
+                Get to know Us
+              </h3>
+              <ul className="flex flex-col gap-2 font-bodyFont">
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="font-titleFont text-white text-base font-semibold mb-3">
+                Get to know Us
+              </h3>
+              <ul className="flex flex-col gap-2 font-bodyFont">
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="font-titleFont text-white text-base font-semibold mb-3">
+                Get to know Us
+              </h3>
+              <ul className="flex flex-col gap-2 font-bodyFont">
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+                <li className="footerLink">Careers</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
